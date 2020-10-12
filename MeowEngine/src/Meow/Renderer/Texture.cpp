8031308d7b667fe9b716dfc008359106f3169433
vedpatil611 <vedpatil611@gmail.com>
2020-10-12 10:01:27 +0530

    Texture sampling

--- MeowEngine/src/Meow/Renderer/Texture.cpp
+++ MeowEngine/src/Meow/Renderer/Texture.cpp
@@ -12,11 +12,22 @@ namespace Meow
 		GLCALL(glGenTextures(1, &m_TexID));
 		GLCALL(glBindTexture(GL_TEXTURE_2D, m_TexID));
 
+		unsigned char * imageBuffer = stbi_load(texPath, &m_Width, &m_Height, &m_BPP, 4);
 
+		GLCALL(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_Width, m_Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, imageBuffer));
+
+		if (imageBuffer)
+			stbi_image_free(imageBuffer);
 	}
 
-	void Texture::bind() const
+	Texture::~Texture()
 	{
+		GLCALL(glDeleteTextures(1, &m_TexID));
+	}
+
+	void Texture::bind(unsigned int slot) const
+	{
+		GLCALL(glActiveTexture(GL_TEXTURE0 + slot));
 		GLCALL(glBindTexture(GL_TEXTURE_2D, m_TexID));
 	}
 
--- MeowEngine/src/Meow/Renderer/Texture.h
+++ MeowEngine/src/Meow/Renderer/Texture.h
@@ -10,12 +10,14 @@ namespace Meow
 	private:
 		unsigned int m_TexID;
 		const char* m_TexPath;
+		int m_Width, m_Height, m_BPP;
 	public:
 		Texture(const char* texPath);
+		~Texture();
 
 		inline unsigned int getTextureId() const { return m_TexID; }
 
-		void bind() const;
+		void bind(unsigned int slot) const;
 		void unbind() const;
 	};
 }
